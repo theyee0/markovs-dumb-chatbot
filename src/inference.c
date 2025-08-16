37f3f7cd46e624a67bac68849c051e61ef28fae2
Jim Chen <theyee0@gmail.com>
2025-08-16 12:33:49 -0700

    Implemented inference function

---
+++ src/inference.c
@@ -0,0 +1,13 @@
+#include "inference.h"
+
+char* next_tok(struct vector* history, const struct table table) {
+        const int prev = vector_peek(history);
+        const int range = table.items[prev][table.size - 1];
+        const int selection = rand() % range;
+        const int index = bsearch(&selection, table.items[prev],
+                                  table.size, sizeof(selection), intcmp);
+
+        vector_push(history, index);
+
+        return lookup_word(index);
+}
