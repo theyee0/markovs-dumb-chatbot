5227ab1de7048cd5bdb29d148d28db56c1d43411
Jim Chen <theyee0@gmail.com>
2025-08-16 12:04:30 -0700

    Implemented translation/interface functions

--- inc/translation.h
+++ inc/translation.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 extern char **vocabulary;
 extern int vocabulary_size;
---
+++ src/translation.c
@@ -0,0 +1,74 @@
+#include "translation.h"
+
+int intcmp(const void *_a, const void *_b) {
+        const int *a = _a;
+        const int *b = _b;
+
+        return *a - *b;
+}
+
+int strcmp_wrap(const void *_a, const void *_b) {
+        const char **a = _a;
+        const char **b = _b;
+
+        return strcmp(*a, *b);
+}
+
+int vector_peek(struct vector v) {
+        return v.items[v.size - 1];
+}
+
+bool vector_push(struct vector *v, int item) {
+        if (v->size >= v->alloc) {
+                v->alloc *= 2;
+                v->items = realloc(&(v->items), v->alloc);
+        }
+
+        v->items[v->size++] = item;
+        return true;
+}
+
+bool load_vocabulary(FILE* fp) {
+        constexpr int buf_size = 1024;
+        char b[buf_size];
+
+        while (fgets(b, buf_size, fp) != NULL) {
+                vocabulary_size++;
+                vocabulary = realloc(&vocabulary, vocabulary_size * sizeof(*vocabulary));
+                vocabulary[vocabulary_size++] = strdup(b);
+        }
+
+        qsort(vocabulary, vocabulary_size, sizeof(*vocabulary), strcmp_wrap);
+}
+
+bool valid_word(char *w) {
+        return bsearch(&w, vocabulary, vocabulary_size, sizeof(*vocabulary), strcmp_wrap);
+}
+
+int fgettok(FILE *fp) {
+        constexpr int buf_size = 1024;
+        char b[buf_size];
+        int len = 0;
+        char c;
+
+        while (!feof(fp) && strchr(" \n\0", c = fgetc(fp)) == NULL)
+                b[len++] = c;
+
+        b[len] = '\0';
+
+        return lookup_tok(b);
+}
+
+int lookup_tok(char* w) {
+        char **loc = bsearch(&w, vocabulary, vocabulary_size, sizeof(*vocabulary), strcmp_wrap);
+
+        if (loc != NULL) {
+                return loc - vocabulary;
+        } else {
+                return -1;
+        }
+}
+
+char* lookup_word(int t) {
+        return vocabulary[t];
+}
